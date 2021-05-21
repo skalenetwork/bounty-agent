4cdf31d9f030e9595a6e4e6cf83879ec10286f62
Alex Sheverdin <alex@skalelabs.com>
2021-05-21 18:56:28 +0300

    SKALE-4198 Add transaction hash to telegram message and log

--- bounty_agent.py
+++ bounty_agent.py
@@ -83,7 +83,7 @@ class BountyAgent:
             raise
         self.logger.info('The bounty was successfully received')
         self.logger.debug(f'Receipt: {tx_res.receipt}')
-        # tx_hash = tx_res.receipt['transactionHash'].hex()
+        tx_hash = tx_res.receipt['transactionHash'].hex()
         self.logger.info(LONG_LINE)
 
         try:
@@ -93,11 +93,12 @@ class BountyAgent:
             args = h_receipt[0]['args']
             bounty_in_skl = self.skale.web3.fromWei(args["bounty"], 'ether')
         except Exception as err:
-            self.notifier.send('Bounty was received, but reward amount cannot be read from '
-                               'tx receipt', MsgIcon.WARNING)
+            self.notifier.send(f'Bounty was received, but reward amount cannot be read from '
+                               f'tx receipt.\nTX hash: {tx_hash}', MsgIcon.WARNING)
             self.logger.exception(err)
         else:
-            self.notifier.send(f'Bounty awarded to node: {bounty_in_skl:.3f} SKL', MsgIcon.BOUNTY)
+            self.notifier.send(f'Bounty awarded to node: {bounty_in_skl:.3f} SKL.\n'
+                               f'TX hash: {tx_hash}', MsgIcon.BOUNTY)
         return tx_res.receipt['status']
 
     @tenacity.retry(wait=tenacity.wait_fixed(RETRY_INTERVAL),
