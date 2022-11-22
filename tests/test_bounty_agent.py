5210bc9c845b3c293d914af08369823e52448a78
badrogger <ivan@skalelabs.com>
2022-11-22 13:49:01 +0000

    Increase mining deley to 1 minute

--- tests/test_bounty_agent.py
+++ tests/test_bounty_agent.py
@@ -30,7 +30,7 @@ from tools.exceptions import NodeNotFoundException
 from tools.helper import check_if_node_is_registered
 
 BLOCK_STEP = 1000
-MINING_DELAY = 20
+MINING_DELAY = 120
 REWARD_DATE_OFFSET = 1000  # additional seconds to skip to ensure reward time is came
 
 
