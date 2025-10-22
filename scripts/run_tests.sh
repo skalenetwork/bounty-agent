e6c7000c5fc8e1dfe9659980b449ce79a4245be6
Dmytro <dmitry.tkk@gmail.com>
2025-10-22 12:32:03 +0100

    Fix private key export for tests

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -8,5 +8,9 @@ export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 mkdir -p $DIR/../skale_vol/contracts_info
 mkdir -p $DIR/../skale_node_data
 
+if [ -z "${ETH_PRIVATE_KEY}" ]; then
+    export ETH_PRIVATE_KEY=$(cat $PWD/helper-scripts/private_key.txt)
+fi
+
 uv run python tests/prepare_validator.py
 ENV=DEV uv run pytest -v -s --cov=./ tests/ --cov-report term-missing
