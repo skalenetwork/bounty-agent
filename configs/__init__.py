ea92d36190d7fae790fd6ea2222993c9fb9ad66d
Alex Sheverdin <alex@skalelabs.com>
2026-02-13 17:46:13 +0000

    Stop using `os.path.join`

--- configs/__init__.py
+++ configs/__init__.py
@@ -9,14 +9,16 @@ ENV = os.environ.get('ENV')
 LONG_LINE = '-' * 100
 
 NOTIFIER_URL = 'http://localhost:3007/send-tg-notification'
-SKALE_VOLUME_PATH = os.getenv('SKALE_VOLUME_PATH', '/skale_vol')
-NODE_DATA_PATH = os.getenv('NODE_DATA_PATH', '/skale_node_data')
+NODE_DATA_FOLDER_NAME: str = 'node_data'
+
+SKALE_VOLUME_PATH: Path = Path(os.getenv('SKALE_VOLUME_PATH', '/skale_vol'))
+NODE_DATA_PATH: Path = SKALE_VOLUME_PATH / NODE_DATA_FOLDER_NAME
 
 NODE_CONFIG_FILENAME = 'node_config.json'
-NODE_CONFIG_FILEPATH = os.path.join(NODE_DATA_PATH, NODE_CONFIG_FILENAME)
+NODE_CONFIG_FILEPATH: Path = NODE_DATA_PATH / NODE_CONFIG_FILENAME
 
 STATE_FILENAME = os.getenv('STATE_FILENAME')
-STATE_BASE_PATH = os.path.join(NODE_DATA_PATH, 'eth-state')
+STATE_BASE_PATH: Path = NODE_DATA_PATH / 'eth-state'
 STATE_FILEPATH = None if not STATE_FILENAME else os.path.join(STATE_BASE_PATH, STATE_FILENAME)
 
 MIN_ETH_AMOUNT_IN_SKL = 0.01
